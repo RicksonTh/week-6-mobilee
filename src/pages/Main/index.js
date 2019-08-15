2d9cbcdaf0c239fe2f2507e13263b09f0fd91f40
Rickson Thompson <ricksoncaique.tecgo@gmail.com>
2019-08-15 16:50:38 -0400

    Main ok

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 
-import styles from './styles';
+import styles from "./styles";
 
-import logo from '../../assets/logo.png';
+import logo from "../../assets/logo.png";
 
 export default class Main extends Component {
   render() {
     return (
-    <View>
+      <View style={styles.container}>
         <Image style={styles.logo} source={logo} />
 
         <TextInput
-        style={styles.input}
-        placeholder="Crie uma box"
-        placeholderTextColor="#999"
-        autoCapitalize="none"
-        autoCorrect={false}
-        underlineColorAndroid="transparent"
+          style={styles.input}
+          placeholder="Crie uma box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
         />
 
-        <TouchableOpacity onPress={() => {}} style={style=styles.button}>
-            <Text style={styles.buttonText}>Criar</Text>
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Criar</Text>
         </TouchableOpacity>
-    </View>
+      </View>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from "react-navigation";
 
-import Main from './pages/Main';
-import Box from './pages/Box';
+import Main from "./pages/Main";
+import Box from "./pages/Box";
 
-const Routes = createAppContainer (
-    //No RC, em todas as rotas vou precisar ter createAppContainer
-    createStackNavigator({
+const Routes = createAppContainer(
+  //No RC, em todas as rotas vou precisar ter createAppContainer
+  createStackNavigator({
     //Tipo de navigation que estou utilizando
-        Main,
-        Box,
-    })
+    Main,
+    Box
+  })
 );
 
 export default Routes;
