a91c943319abfb6f01f5852dbf7398c895653451
Rickson Thompson <ricksoncaique.tecgo@gmail.com>
2019-08-22 16:57:04 -0400

    Botão de upload inserido na page box

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,11 +1,79 @@
 import React, { Component } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import ImagePicker from "react-native-image-picker";
 
-import { View } from "react-native";
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import api from "../../services/api";
+
+import Icon from "react-native-vector-icons";
+
+import styles from "./styles";
 
 export default class Box extends Component {
-  render() {
-    return <View />;
+  state = { box: {} };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@Mobille:box");
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
   }
+
+  handleUpload = () => {
+    ImagePicker.launchImageLibrary({}, async upload => {
+      const data = new FormData();
+      const [prefix,suffix] = upload.fileName.split(".");
+      const ext = suffix.toLowerCase() === "heic" ? "jpg" : suffix;
+
+      data.append("file", {
+        uri: upload.uri,
+        type: upload.type,
+        name: `${prefix}.${ext}`
+      });
+
+      api.post(`boxes/${this.state.box._id}`, data);
+    }
+    });
+  };
 }
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => } style={styles} >
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileDate}>
+        há{""}
+        {distanceInWords(item.createdAt, new Date(), {
+          locale: pt
+        })}
+      </Text>
+    </TouchableOpacity>
+  );
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+        <FlatList
+         style={styles.list}
+         data={this.state.box.files}
+         keyExtractor={file => file._id}
+    ItemSeparatorComponent={() => <View style={styles.separator} /> }
+         renderItem={this.renderItem}
+        />
+
+       <TouchableOpacity style={styles.fab} onPress={this.handleUpload}>
+        <Icon name="cloud-upload" size={24} color="#FFF" />
+      </TouchableOpacity>
+      </View>
+    );
+}
+}
+
+
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from "@react-native-community/async-storage";
 
-import api from '../../services/api';
+import api from "../../services/api";
 
 import styles from "./styles";
 
@@ -10,21 +10,31 @@ import logo from "../../assets/logo.png";
 
 export default class Main extends Component {
   state = {
-    newBox: ''
+    newBox: ""
   };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@Mobilee:box");
+
+    if (box) {
+      this.props.navigation.navigate("Box");
+    }
+  }
 
   handleSignIn = async () => {
     //Quando o user clicar no botão
     const response = await api.post("boxes", {
       //response representa a requisição a api
       //o método deve ser igual ao que está na api(post, get etc)
-        title: this.state.newBox
-        //Aqui vai o corpo da requisição. Não precisa ser em json, pois o axios já converte
-      });
-    
-      this.props.navigation.navigate('Box');
-      //No mobile, vai ser a prop navigation. O navigate usa o nome da rota por onde vai navegar
-  }
+      title: this.state.newBox
+      //Aqui vai o corpo da requisição. Não precisa ser em json, pois o axios já converte
+    });
+
+    await AsyncStorage.setItem("@Mobilee:box", response.data._id);
+
+    this.props.navigation.navigate("Box");
+    //No mobile, vai ser a prop navigation. O navigate usa o nome da rota por onde vai navegar
+  };
 
   render() {
     return (
@@ -36,10 +46,10 @@ export default class Main extends Component {
           placeholder="Crie uma box"
           placeholderTextColor="#999"
           autoCapitalize="none"
-          autoCorrect={false}  
+          autoCorrect={false}
           underlineColorAndroid="transparent"
           value={this.state.newBox}
-          onChangeText = { text => this.setState({ newBox: text})}
+          onChangeText={text => this.setState({ newBox: text })}
         />
 
         <TouchableOpacity onPress={this.handleSignIn} style={styles.button}>
